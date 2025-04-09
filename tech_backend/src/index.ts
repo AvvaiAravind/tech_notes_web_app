48f2f366689398da7aea408eaeceaa644f3e85cd
Avvai Aravind <avvaiaravind@gmail.com>
2025-04-09 22:36:33 +0530

    create filename and dirname for use

--- tech_backend/src/index.ts
+++ tech_backend/src/index.ts
@@ -1,15 +1,23 @@
-import "#env"; // Import environment variables from .env file
-import express from "express";
+import '#env'; // Import environment variables from .env file
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// since es modules we need to create filename and dirname manually
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3500;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 // Basic route to test the server
-app.get("/", (_req, res) => {
-  res.send("Backend is running!");
+app.get('/', (_req, res) => {
+  res.send('Backend is running!');
 });
 
 // Start the server
