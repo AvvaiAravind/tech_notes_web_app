c79454213a18a77e9b9b06c34578988a6174cdee
Avvai Aravind <avvaiaravind@gmail.com>
2025-05-03 17:01:58 +0530

    modified index css for theme

--- tech_frontend/src/App.tsx
+++ tech_frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex flex-grow items-center justify-center">
-      <p className="bg-blue-600 p-4 text-center text-5xl text-white">
+      <p className="bg-info-blue p-4 text-center text-5xl text-white">
         Vite Template
       </p>
     </div>
