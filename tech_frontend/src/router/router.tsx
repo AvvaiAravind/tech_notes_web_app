7222c59d37071de7140b9f9ea8748e060fc95088
Avvai Alias Aravind <aravindr@insytix.com>
2025-05-12 15:59:25 +0530

    updated react router from react router dom

--- tech_frontend/src/router/router.tsx
+++ tech_frontend/src/router/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 import DashLayout from "src/layouts/DashLayout";
-import Login from "src/pages/login/Login";
-import Public from "src/pages/pub/Public";
 import Layout from "src/layouts/Layout";
+import Login from "src/pages/login/Login";
 import NotesList from "src/pages/notes/NotesList";
+import Public from "src/pages/pub/Public";
 import UsersList from "src/pages/users/UsersList";
 
 const router = createBrowserRouter([
